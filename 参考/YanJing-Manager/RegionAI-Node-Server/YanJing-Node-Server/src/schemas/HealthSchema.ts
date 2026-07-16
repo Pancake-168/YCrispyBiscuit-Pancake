16c7b5271b14478bed9ce2f191af4e65a83876cf
YCrispBiscuit <13513718436@163.com>
2026-07-16 17:44:37 +0800

    删除RegionAI-Node-Server

--- 参考/YanJing-Manager/RegionAI-Node-Server/YanJing-Node-Server/src/schemas/HealthSchema.ts
+++
@@ -1,9 +0,0 @@
-import { z } from '@hono/zod-openapi'
-
-export const HealthResponseSchemaObj = z
-  .object({
-    status: z.string().openapi({ example: 'ok' })
-  })
-  .openapi('HealthResponseSchema')
-
-export type HealthResponseSchema = z.infer<typeof HealthResponseSchemaObj>
--- 参考/YanJing-Manager/RegionAI-Node-Server/参考/Server/Python-FastAPI/app/schemas/HealthSchema.py
+++
@@ -1,5 +0,0 @@
-from pydantic import BaseModel
-from pydantic import ConfigDict
-class HealthResponse(BaseModel):
-    model_config = ConfigDict(from_attributes=True)
-    status: str
